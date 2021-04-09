9b8708428d586aeb56956d4e742a9642e7c4193e
Alex Corrado <alexander.corrado@gmail.com>
2021-04-08 20:17:29 -0400

    Add test for RFC FS-1079

---
+++ tests/FSharp.Compiler.ComponentTests/Language/DiscriminatedUnionTests.fs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+
+namespace FSharp.Compiler.ComponentTests.Language
+
+open Xunit
+open FSharp.Test.Utilities.Compiler
+open FSharp.Quotations.Patterns
+
+module DiscriminatedUnionTests =
+
+    [<Fact>]
+    let ``Is* discriminated union properties are visible`` () =
+        FSharp """
+namespace rec Hello
+
+[<Struct>]
+type Foo =
+    private
+    | Foo of string
+    | Bar
+
+module Main =
+    [<EntryPoint>]
+    let main _ =
+        let foo = Foo.Foo "hi"
+        printfn "IsFoo: %b / IsBar: %b" foo.IsFoo foo.IsBar
+        0
+        """
+        |> compileExeAndRun
+        |> shouldSucceed
+        |> withStdOutContains "IsFoo: true / IsBar: false"
